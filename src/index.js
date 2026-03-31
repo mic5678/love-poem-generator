caafffb796e03cf92eed7bbd49521173e0e7b161
Micah <evansmicah88@gmail.com>
2026-03-31 17:58:40 -0400

    assign user instructions and api key and axios

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "Love is beautiful",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "7ao9826852a4b8038td2ff4e87b113f2";
+  let prompt = `User instructions: Generate a love poem about ${instructionsInput.value}`;
+  let context =
+    "You are a romantic Poem expert and loves writing short poems. Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem.";
+  let apiURL = ` https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiURL).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#love-poem-generator-form");
