8efbeba96f2bf03b0ffe259e8eb4fa2761e64c47
Micah <evansmicah88@gmail.com>
2026-03-31 16:56:19 -0400

    updated string

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-    function generatePoem(event) {
-        event.preventDefault();
-        
-new Typewriter("#poem", {
-  strings: ,
-  autoStart: true,
-  delay:1,
-  cursor:"",
-});
-    }
-    
-let poemFormElement = document.querySelector("#love-poem-generator-form")
+function generatePoem(event) {
+  event.preventDefault();
+
+  new Typewriter("#poem", {
+    strings: "Love is beautiful",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#love-poem-generator-form");
 poemFormElement.addEventListener("submit", generatePoem);
