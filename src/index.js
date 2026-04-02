40df19d8f4801307552fae62a87496d46a1cf0f3
Micah <evansmicah88@gmail.com>
2026-04-01 20:53:38 -0400

    changed AI context

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-
-function generatePoem(event) {
-  event.preventDefault(); 
+function displayPoem(response) {
   new Typewriter("#poem", {
     strings: response.data.answer,
     autoStart: true,
@@ -9,12 +7,14 @@ function generatePoem(event) {
   });
 }
 
+function generatePoem(event) {
+  event.preventDefault();
 
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "7ao9826852a4b8038td2ff4e87b113f2";
   let prompt = `User instructions: Generate a love poem about ${instructionsInput.value}`;
   let context =
-    "You are a romantic Poem expert and loves writing short poems. Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem.";
+    "You are a romantic Poem expert and loves writing short poems. Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Provide the code only, without markdown formatting, backticks, or language name. Make sure to follow the user instructions. Do not include a title to the poem.";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let poemFormElement = document.querySelector("#poem");
